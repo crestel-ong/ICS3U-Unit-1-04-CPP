cf84f748c90b741da19f16ed2d1dfd4abae866f4
crestel-ong <crestel.ong@mths.ca>
2021-09-14 01:18:03 +0000

    please be the last time

--- basicMath.cpp
+++ basicMath.cpp
@@ -1,20 +1,18 @@
 // Copyright (c) 2021 Crestel Ong All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Crestel Ong
+// Created on: Sep 2021
 // This program calculates the area and perimeter of a rectangle
-//     with dimensions 5cm x 3cm
-
+//    with dimensions 5cm x 3cm
 
 #include <iostream>
-#include <math.h>       /* pow */
 
 int main() {
     std::cout << "If a rectangle has the dimensions: " << std::endl;
     std::cout << "5cm x 3cm" << std::endl;
     std::cout << std::endl;
-    std::cout << "Area is " << (5 * 3) << "cm2." << std::endl;
-    std::cout << "Perimeter is " << (2 *(5 + 3)) << "cm." << std::endl;
+    std::cout << "Area is " << (5 * 3) << "cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (5 + 3)) << "cm." << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
